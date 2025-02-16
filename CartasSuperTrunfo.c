8b934d6e183b4232f5b14200afc22f83a264d389
Ancelmo Pinheiro <ancelmophi@gmail.com>
2025-02-16 07:54:27 +0000

    Ancelmo - desafio nível 1

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,9 +7,45 @@
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
 
+  int populacao, pontos_turisticos;
+  char estado, codigo_carta[20], nome_cidade[30];
+  float area, pib;
+
   // Área para entrada de dados
 
+  printf("Digite o estado: ");
+  scanf("%c", &estado);
+
+  printf("Digite o código da carta: ");
+  scanf("%s", &codigo_carta[20]);
+
+  printf("Digite o nome da cidade: ");
+  scanf("%s", &nome_cidade[30]);
+
+  printf("Digite a população: ");
+  scanf("%d", &populacao);
+
+  printf("Digite a área: ");
+    scanf("%f", &area);
+
+  printf("Digite o PIB: ");
+  scanf("%f",&pib);
+
+  printf("Digite o número de pontos turísticos: ");
+  scanf("%d", &pontos_turisticos);
+
   // Área para exibição dos dados da cidade
+
+  
+  printf("Estado: %c\n Código da carta: %s\n Nome da cidade: %s\n", estado, codigo_carta, nome_cidade);
+
+  printf("População: %d\n", populacao);
+
+  printf("Área: %f km²\n", area);
+
+  printf("PIB: %f\n", pib);
+
+  printf("Número de pontos turísticos: %d\n", pontos_turisticos);
 
 return 0;
 } 
